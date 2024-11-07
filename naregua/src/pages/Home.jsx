0217805c95f63c4768cf75d484d9abb094c27ddb
davymaciel <davymaciel700@gmail.com>
2024-11-07 10:56:42 -0300

    alterações na home

--- naregua/src/pages/Home.jsx
+++ naregua/src/pages/Home.jsx
@@ -8,34 +8,37 @@ import Contato from './Contato';
 import "./Home.css";
 
 const Home = () => {
-    const [CtState, dispatch] = useContext(CtContext);
+  const [CtState, dispatch] = useContext(CtContext);
 
-return (
-<div className='container'>
-  <div className='navbar'>
-    <img src={logoImage} alt="" className="logo" />
-      <ul>
-        <li>Sobre</li>
-        <li>Serviços</li>
-        <li>Equipe</li>
-        <li>Contato</li>       
-      </ul>        
+  return (
+    <div className='container'>
+      <div className='navbar'>
+        <img src={logoImage} alt="" className="logo" />
+        <ul>
+          <li>Sobre</li>
+          <li>Serviços</li>
+          <li>Equipe</li>
+          <li>Contato</li>
+        </ul>
         <div className="buttonsLC">
-            <button onClick={() => dispatch({ type: 'LOGIN_STAGE' })}>Login</button>
-            <button onClick={() => dispatch({ type: 'CADASTRO_STAGE' })}>Cadastro</button>
-        </div>      
-  </div>
+          <button onClick={() => dispatch({ type: 'LOGIN_STAGE' })}>Login</button>
+          <button onClick={() => dispatch({ type: 'CADASTRO_STAGE' })}>Cadastro</button>
+        </div>
 
-            <button onClick={() => dispatch({ type: 'AGENDAMENTO_STAGE' })}>Agende seu Horário</button> 
-  <div>
-  <Sobre />
-  <Serviços />
-  <Equipe />
-  <Contato />
-  </div>
-</div>
-  
-)
+
+      </div>
+
+
+      
+      <div>
+        <Sobre />
+        <Serviços />
+        <Equipe />
+        <Contato />
+      </div>
+    </div>
+
+  )
 }
 
 export default Home
--- naregua/src/pages/Sobre.jsx
+++ naregua/src/pages/Sobre.jsx
@@ -1,9 +1,13 @@
 const Sobre = () => {
 
     return (
-        <div>
-            <h1>beleza e autoestima também são importantes para os homens.</h1>
-        </div>
+        <div className= 'container-home'>
+
+    <p>NaRégua Barbearia é mais que uma barbearia,buscamos <br/>oferecer a você uma experiência de cuidado e atenção aos detalhes. 
+        <br/>Nossa equipe é formada por profissionais dedicados a <br/>te deixar no estilo. Agende já seu horário e venha ficar NaRégua!</p>
+        <button onClick={() => dispatch({ type: 'AGENDAMENTO_STAGE' })}>Agende seu Horário</button>
+  </div>
+  
     )
 }
 
