95d66b00850106507699fa5f41781ef9883bafec
davymaciel <davymaciel700@gmail.com>
2024-11-08 10:53:19 -0300

    alterando fontes e background

--- naregua/src/pages/Sobre.jsx
+++ naregua/src/pages/Sobre.jsx
@@ -1,15 +1,13 @@
 const Sobre = () => {
 
     return (
-        
         <div className= 'container-home'>
 
-    <p>NaRégua Barbearia é mais que uma barbearia,buscamos <br/>oferecer a você uma experiência de cuidado e atenção aos detalhes. 
-        <br/>Nossa equipe é formada por profissionais dedicados a <br/>te deixar no estilo. Agende já seu horário e venha ficar NaRégua!</p>
+    <p>NaRégua BARBEARIA É MAIS QUE UMA BARBEARIA, <br/>BUSCAMOS OFERECER A VOCÊ UMA EXPERIÊNCIA<br/>DE CUIDADO E ATENÇÃO AOS DETALHES. 
+    <br/>AGENDE JÁ SEU HORÁRIO E <br/> VENHA FICAR NaRégua!</p>
         <button onClick={() => dispatch({ type: 'AGENDAMENTO_STAGE' })}>Agende seu Horário</button>
   </div>
-  
     )
 }
-
+    
 export default Sobre
