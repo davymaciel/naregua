6a2650fa4b397283fc9c40e70748b80348d86ae6
davidmartinsperes <davidmartinsperes@gmail.com>
2024-11-14 01:33:35 -0300

    HP ATT2

--- naregua/src/pages/Equipe.jsx
+++ naregua/src/pages/Equipe.jsx
@@ -1,10 +1,48 @@
+import { useState, useEffect, useRef } from 'react'
+import { motion } from 'framer-motion'
+import b1 from './img/b1.jfif'
+import b2 from './img/b2.jpg'
+import b3 from './img/b3.png'
+
+
+const images = [b1, b2, b3]
+
 const Equipe = () => {
+    const carousel = useRef();
+    const [width, setWidth] = useState(0)
+
+    useEffect(() => {
+        console.log(carousel.current?.scrollWidth, carousel.current?.offsetWidth)
+        setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth)
+    }, [])
+
 
     return (
-        <div>
-            <h1>Equipe</h1>
+        <div className='Equipe'>
+            
+
+            <motion.div ref={carousel} className='carousel' whileTap={{ cursor: "grabbing" }}>
+            <h1>Nossa Equipe</h1>
+                <motion.div
+                    className='inner'
+                    drag="x"
+                    dragConstraints={{ right: 0, left: -width }}
+                    initial={{ x: 100 }}
+                    animate={{ x: 0 }}
+                    transition={{ duration: 0.8 }}>
+
+                    {images.map(image => (
+                        <motion.div className='item' key={image}>
+                            <img src={image} alt="Texto" />
+                        </motion.div>
+                    ))}
+
+                </motion.div>
+               
+            </motion.div>
+            
         </div>
-    )
+    );
 }
 
 export default Equipe
